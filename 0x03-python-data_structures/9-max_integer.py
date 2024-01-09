7083964cbf33533cf0ed05bd920675e33d67e35d
AmnaHussien <amnahussin88@gmail.com>
2024-01-09 14:12:38 -0800

    k

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+  * max_integer - function that finds the biggest integer of alist
+  * my_list: lists
+  * Return: biggest integer in the lists
+  *
+ */
+def max_integer(my_list=[]):
+    for i in range(len(my_list=[])):
+        if i < i + 1:
+            continue
+        else:
+            print(i)
+        i = i +1
---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,4 @@
+#ifndef "MAIN_H"
+#define MAIN_H
+def max_integer(my_list=[]);
+#endif
