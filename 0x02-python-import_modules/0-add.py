a957d82b196e0c394b327f2719a0c16d33a70e3e
AmnaHussien <amnahussin88@gmail.com>
2024-01-10 15:19:39 -0800

    k

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+    i = 1
+    j = 2
+    print("{} + {} = {}".format(i, j, add(i, j)))
