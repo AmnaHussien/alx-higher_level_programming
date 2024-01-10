48539589496d37cfd99dbf71caa698f1fa577635
AmnaHussien <amnahussin88@gmail.com>
2024-01-10 15:39:33 -0800

    k

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+    i = 10
+    j = 5
+    print("{} + {} = {}".format(i, j, add(i, j)))
+    print("{} - {} = {}".format(i, j, sub(i, j)))
+    print("{} * {} = {}".format(i, j, mul(i, j)))
+    print("{} / {} = {}".format(i, j, div(i, j)))
