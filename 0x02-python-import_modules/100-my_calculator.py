c566f0b0195f2117434b5ce60ed36b1a4de413c9
AmnaHussien <amnahussin88@gmail.com>
2024-01-10 17:04:53 -0800

    k

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,15 +7,17 @@ if __name__ == "__main__":
 
 
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <i> <operator> <j>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    i = int(sys.argv[1])
-    j = int(sys.argv[3])
-    print("{} {} {} = {}".format(i, sys.argv[2], j, ops[sys.argv[2]](i, j)))
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
 
