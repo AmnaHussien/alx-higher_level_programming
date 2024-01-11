d4e18e89c2be4d006a806aa4d4e8cf7ef0e6aa83
AmnaHussien <amnahussin88@gmail.com>
2024-01-10 17:39:36 -0800

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,9 +5,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <i> <operator> <j>")
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
@@ -15,7 +14,6 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    i = int(sys.argv[1])
+    j = int(sys.argv[3])
+    print("{} {} {} = {}".format(i, sys.argv[2], j, ops[sys.argv[2]](i, j)))
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -11,4 +11,4 @@ def magic_calculation(a, b):
             c = add(c, j)
         return (c)
     else:
-        return(sub(a, b))
+        return (sub(a, b))
