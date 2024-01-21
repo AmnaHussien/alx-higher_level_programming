db6221681a5e2f104126a3794a57cdaa4c999a15
AmnaHussien <amnahussin88@gmail.com>
2024-01-21 13:06:39 -0800

    k

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+i = "abcdefghijklmnopqrstuvwxyz"
+for index in range(len(i)):
+    if i[index] == 'q' or i[index] == 'e':
+        continue
+    print(i[index], end = "")
+    index = index + 1
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+for digit in range(0, 10):
+        for ch in range(0, 10):
+             if digit == ch or digit > ch:
+                 continue
+             print(f"{digit}{ch}" + ", " , end = "")
+             ch = ch + 1
+             if digit > ch:
+                 print(f"{digit}{ch}" )
+                 break
+        digit = digit + 1
