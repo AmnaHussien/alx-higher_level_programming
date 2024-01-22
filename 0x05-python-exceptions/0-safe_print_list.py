a9cee81130704291eb457dcf35edefdca52369ba
AmnaHussien <amnahussin88@gmail.com>
2024-01-22 13:59:41 -0800

    d

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-try:
-    def safe_print_list(my_list=[], x=0):
-        for i in range(x):
+def safe_print_list(my_list=[], x=0):
+    m = 0
+    for i in range(x):
+        try:
             print(my_list[i], end = "")
-        print("\n")
-        print("nb_print:", x)
-except:
-    print("\n")
+            m += 1
+        except IndexError:
+            break;
+    print("")
+    return (m)
