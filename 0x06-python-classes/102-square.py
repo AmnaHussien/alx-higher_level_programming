7bc1403497f7a1d125d7947413596195119ce9ea
AmnaHussien <amnahussin88@gmail.com>
2024-01-24 12:51:39 -0800

    d

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -21,5 +21,16 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of square."""
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        """Define the == comparision to Square."""
+        return self.area() == other.area()
 
