4c1b9db974e82c7d1897f3594ca0e404785cea98
AmnaHussien <amnahussin88@gmail.com>
2024-02-19 16:17:20 -0800

    g

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+class
+"""
+
+
+class BaseGeometry:
+    """ class base """
+    def area(self):
+        """ area function """
+        raise Exception("area() is not implemented")
