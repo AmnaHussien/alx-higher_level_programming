c6a0d2470019ee81e4d2b8ab47e0c248eb9f451a
AmnaHussien <amnahussin88@gmail.com>
2024-02-21 08:55:20 -0800

    d

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,8 @@ function to write in file
 
 def write_file(filename="", text=""):
     """ contains write in file and writern the number of character written """
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "r+", encoding="utf-8") as file:
         data_written = file.write(text)
         print(data_written, end="")
+        data = file.read()
+    print(data)
