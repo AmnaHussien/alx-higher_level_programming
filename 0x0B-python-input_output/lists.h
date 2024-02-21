7a8b8b98b7a9529a532aa0d81a26d028c46a2ca9
AmnaHussien <amnahussin88@gmail.com>
2024-02-21 14:58:41 -0800

    d

---
+++ 0x0B-python-input_output/lists.h
@@ -0,0 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+def read_file(filename="");
+def write_file(filename="", text="");
+def append_write(filename="", text="");
+def to_json_string(my_obj);
+def from_json_string(my_str);
+def save_to_json_file(my_obj, filename);
+def load_from_json_file(filename);
+def class_to_json(obj);
+def __init__(self, first_name, last_name, age);
+def __init__(self, first_name, last_name, age);
+def pascal_triangle(n);
+#endif
