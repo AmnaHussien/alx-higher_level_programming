21fb586ee3ce1d6d68fba6310fc882315b415935
AmnaHussien <amnahussin88@gmail.com>
2024-02-21 07:18:10 -0800

    d

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+read file function
+"""
+
+
 def read_file(filename=""):
-    """ function that read file """
-
-
-    with open("", encoding = "utf-8") as file:
+    """ function that read utf-8 and print it to stdout """
+    with open(filename, "r", encoding = "utf-8") as file:
         data = file.read()
-    print(data)
+        print(data, end = "")
