0c922a7d7e6086213cbc181b8df02f0bc7bc4f8d
AmnaHussien <amnahussin88@gmail.com>
2024-02-21 09:42:31 -0800

    d

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@ function to append in file
 
 
 def append_write(filename="", text=""):
-    """ contains append in file and writern the number of added character"""
-    with open(filename, "a", encoding="utf=8") as file:
+    """ contains append in file and retrn the number of added character"""
+    with open(filename, "a", encoding="utf-8") as file:
         data = file.append(text)
         return(data)
