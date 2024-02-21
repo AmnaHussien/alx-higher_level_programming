87d89cd96639118de6114c68f31c2102a223fc7e
AmnaHussien <amnahussin88@gmail.com>
2024-02-21 07:28:08 -0800

    g

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function to write in file
+"""
+
+
+def write_file(filename="", text=""):
+    """ contains write in file and writern the number of character written """
+    with open(filename, "w", encoding="utf-8") as file:
+        data_written = file.write(text)
+    print(data_written)
