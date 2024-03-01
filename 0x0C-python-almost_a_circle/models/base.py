eddfeee3887b4bdbb5087f456d143c5151608236
AmnaHussien <amnahussin88@gmail.com>
2024-03-01 15:00:14 -0800

    d

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,9 +7,10 @@ contain classes
 class Base:
     __nb_objects = 0
 
-    def __init__(self, id=None):
+    def __init__(self, _id=None):
         " initialization "
-        if id is not None:
-            id = id-1
-        else:
+        if id is  None:
             Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = _id
