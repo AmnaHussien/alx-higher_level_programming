9b58271add51d2a957f3470984ee3f966138ad16
AmnaHussien <amnahussin88@gmail.com>
2024-07-06 09:59:24 -0700

    g

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,4 +22,6 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
