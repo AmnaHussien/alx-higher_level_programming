3d4c6fb5bdcd1c694512a3ebd6b8df34948b98e6
AmnaHussien <amnahussin88@gmail.com>
2024-07-06 10:02:46 -0700

    f

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,10 +11,8 @@ class square:
             args:
                 size(int): the size of new Square
         """
-        if size in int.__class__:
-            pass
-        else:
+        if not isinstance(size, int):
             raise TypeErro("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
